e50511ecd06196961b03f12e8bedf814bc26d64e
Michael Wang <michael.wang@myob.com>
2020-09-14 14:54:30 +1000

    Implemented logic and TDD up to Step 9

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace StringCalculator
 {
@@ -6,7 +8,23 @@ namespace StringCalculator
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("//;\n1;2".Substring(0,4));
+            var calculator = new StringCalculator();
+            var str = "[*][%]";
+            var arr = str.Split(new string[] {
+                "][",
+                "[",
+                "]",
+            }, StringSplitOptions.None);
+            
+            var list = new List<string>();
+            foreach(var item in arr)
+            {
+                if(item.Length > 0)
+                {
+                    list.Add(item);
+                }
+            }
+            System.Console.WriteLine(list.Count);
         }
     }
 }
--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+
 namespace StringCalculator
 {
     public class StringCalculator
@@ -13,49 +15,82 @@ namespace StringCalculator
             var total = 0;
             if (String.IsNullOrEmpty(text)) return total;
 
-            if (text[0] == '/')
+            if (text.Contains("//"))
             {
-                var delimiter = this.IsolateDelimiter(text);
-                var newText = text.Split(new string[]
-                    {
-                        $"//{delimiter}\n"
-                    }, StringSplitOptions.None
-                )[1];
-
-                char[] delimiters = new char[] {
-                    ',',
-                    '\n',
-                    delimiter
-                };
-
-                var stringNumbers = newText.Split(delimiters);
-                
-                foreach(string number in stringNumbers)
-                {
-                    total += Convert.ToInt32(number);
-                }
+                var delimiter = IsolateDelimiter(text);
+                var newText = FormatString(text, delimiter);
+                string[] delimiters = CreateDelimitersArray(delimiter);
+                var stringNumbers = newText.Split(delimiters, StringSplitOptions.None);
+                total = SumStringNumbersInArray(newText, total, delimiters);
             }
             else
             {
-                char[] delimiters = new char[] {
-                    ',',
-                    '\n'
-                };
-
-                var stringNumbers = text.Split(delimiters);
-                
-                foreach(string number in stringNumbers)
-                {
-                    total += Convert.ToInt32(number);
-                }
+                string[] delimiters = CreateDelimitersArray();
+                total = SumStringNumbersInArray(text, total, delimiters);
             }
 
            return total;
         }
 
-        public char IsolateDelimiter(string text)
+        public int SumStringNumbersInArray(string text, int total, string[] delimiters)
         {
-            return text[2];
+            var stringNumbers = text.Split(delimiters, StringSplitOptions.None);
+            var negativeStringNumbers = new List<string>();
+            foreach(string number in stringNumbers)
+            {
+                if (Convert.ToInt32(number) < 0)
+                {
+                    negativeStringNumbers.Add(number);
+                }
+                if (Convert.ToInt32(number) < 1000)
+                {
+                    total += Convert.ToInt32(number);
+                }
+            }
+
+            if (negativeStringNumbers.Count > 0)
+            {
+                throw new ArgumentException($"Throws exception with Negatives not allowed: {String.Join(", ", negativeStringNumbers)}");
+            }
+
+            return total;
+        }
+
+        public string IsolateDelimiter(string text)
+        {
+            var end = text.IndexOf("\n") - 2;
+            var delimiter = text.Substring(2,end);
+            if(delimiter[0] == '[')
+            {   
+                // CONVERT TO ARRAY
+                return delimiter.Substring(1, delimiter.Length - 2);
+            }
+            return text.Substring(2,end);
+        }
+
+        public string FormatString(string text, string delimiter)
+        {
+            if (delimiter.Length > 1)
+            {
+                delimiter = $"[{delimiter}]";
+            }
+            // CONVERT ARRAY INTO NEW DELIMITER
+            var newDelimiter = $"//{delimiter}\n";
+
+            return text.Split(new string[]
+                {
+                newDelimiter
+                }, StringSplitOptions.None
+            )[1];
+        }
+
+        public string[] CreateDelimitersArray(string delimiter = ",")
+        {
+            return new string[] {
+                ",",
+                "\n",
+                delimiter
+            };
         }
     }
 }
